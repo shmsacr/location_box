e33b2efeb8f8d6d68bf49ebaedf7ad24673ddd41
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-12-17 20:52:14 +0300

    Update home_view.dart with localization and theme changes

--- lib/app/view/home/view/home_view.dart
+++ lib/app/view/home/view/home_view.dart
@@ -1,31 +1,39 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:location_box/app/product/init/custom_localization.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:location_box/app/product/init/localization/app_localization.dart';
+import 'package:location_box/app/product/init/state/theme/view_model.dart';
 import 'package:location_box/app/product/utility/constants/enum/locales.dart';
-import 'package:location_box/product/init/language/locale_keys.g.dart';
+
+import '../../../product/init/localization/locale_keys.g.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          CustomLocalization.updateLanguage(
-              locales: Locales.en, context: context);
-        },
-        child: const Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        title: Text('Material App Bar'),
-      ),
-      body: Center(
-        child: Card(
-          child: Container(
-            width: 200,
-            height: 200,
-            child: Text(LocaleKeys.general_button_ok).tr(),
+    return BlocProvider(
+      create: (context) => AppThemeViewModel(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => AppLocalization.updateLanguage(
+              locales: Locales.tr, context: context),
+          child: const Icon(Icons.add),
+        ),
+        appBar: AppBar(
+            title: Text('Material App Bar'),
+            leading: IconButton(
+              onPressed: context.read<AppThemeViewModel>().changeThemeMode,
+              icon: const Icon(Icons.add),
+            )),
+        body: Center(
+          child: Card(
+            child: Container(
+              alignment: Alignment.center,
+              width: 200,
+              height: 200,
+              child: Text(LocaleKeys.general_button_ok).tr(),
+            ),
           ),
         ),
       ),
