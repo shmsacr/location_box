5807446d3203ec6b185810350a875e9eb1a0770c
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-12-17 17:09:42 +0300

    Add localization and theme support

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:location_box/app/product/init/custom_localization.dart';
+import 'package:location_box/app/product/theme/dark_theme_data.dart';
+import 'package:location_box/app/product/theme/light_theme_data.dart';
+import 'package:location_box/app/view/home/view/home_view.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(CustomLocalization(child: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      themeMode: ThemeMode.light,
+      theme: CustomLightTheme().themeData,
+      darkTheme: CustomDarkTheme().themeData,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Location Box'),
-          ),
-        ),
-      ),
+      home: HomeView(),
     );
   }
 }
