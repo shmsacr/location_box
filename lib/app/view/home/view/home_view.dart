5c12beecff6f1dbd37f11c3a09dfcc1af2832587
Sehmus Acar <shmsacr@gmail.com>
2023-12-21 20:57:53 +0300

    Add drawer and floating action button to HomeView

--- lib/app/view/home/view/home_view.dart
+++ lib/app/view/home/view/home_view.dart
@@ -2,9 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:location_box/app/product/init/localization/app_localization.dart';
 import 'package:location_box/app/product/init/state/theme/view_model.dart';
-import 'package:location_box/app/product/utility/constants/enum/locales.dart';
+import 'package:location_box/app/product/navigation/app_router.dart';
 
 import '../../../product/init/localization/locale_keys.g.dart';
 
@@ -17,17 +16,32 @@ final class HomeView extends StatelessWidget {
     return BlocProvider(
       create: (context) => AppThemeViewModel(),
       child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => AppLocalization.updateLanguage(
-              locales: Locales.tr, context: context),
-          child: const Icon(Icons.add),
+          onPressed: () => context.router.push(GoogleMapsRoute()),
+          child: Icon(Icons.add),
         ),
         appBar: AppBar(
-            title: Text('Material App Bar'),
-            leading: IconButton(
-              onPressed: context.read<AppThemeViewModel>().changeThemeMode,
-              icon: const Icon(Icons.add),
-            )),
+          title: Text('Material App Bar'),
+        ),
         body: Center(
           child: Card(
             child: Container(
