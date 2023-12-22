967a67700f42310a716d37f7e91101cabb7e97c2
Sehmus Acar <shmsacr@gmail.com>
2023-12-22 18:14:02 +0300

    Add FormNameEnum for map view

---
+++ lib/app/view/maps/enum/form_builder_name_enum.dart
@@ -0,0 +1,12 @@
+enum FormNameEnum {
+  title('title'),
+  description('description'),
+  address('address'),
+  image('image'),
+  phone('phone'),
+  latitude('latitude'),
+  longitude('longitude');
+
+  final String value;
+  const FormNameEnum(this.value);
+}
