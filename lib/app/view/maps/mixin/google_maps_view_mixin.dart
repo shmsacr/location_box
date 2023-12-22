7315ea2e5c057e644946ac2e762338b25032e590
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-12-22 03:00:13 +0300

    Add text editing controllers and update location retrieval

--- lib/app/view/maps/mixin/google_maps_view_mixin.dart
+++ lib/app/view/maps/mixin/google_maps_view_mixin.dart
@@ -1,37 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location_box/app/view/maps/view/google_maps_view.dart';
 import 'package:location_box/app/view/maps/view_model/google_maps_view_model.dart';
 
 mixin GoogleMapsViewMixin on State<GoogleMapsView> {
+  final TextEditingController addressController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController imageController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+
   late GoogleMapController mapController;
+  final TextEditingController titleController = TextEditingController();
   final formKey = GlobalKey<FormBuilderState>();
-  final mapsViewModel = GoogleMapsViewModel();
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationMaps();
   }
 
   @override
   void dispose() {
     formKey.currentState?.reset();
+    addressController.dispose();
+
+    descriptionController.dispose();
+    imageController.dispose();
+    titleController.dispose();
+
     super.dispose();
   }
 
-  Future<void> getLocation() async {
-    await mapsViewModel.getCurrentLocation();
-    print(
-        'State : ${mapsViewModel.state.latitude}, ${mapsViewModel.state.longitude},${mapsViewModel.state.currentLocation}}');
+  Future<void> getLocationMaps() async {
+    await context.read<GoogleMapsViewModel>().getCurrentLocation();
   }
 
-  Set<Marker> createMarker() {
+  Set<Marker> createMarker({required LatLng position}) {
     return {
       Marker(
         markerId: MarkerId("current_location"),
-        position: mapsViewModel.state.currentLocation!,
+        position: position,
         infoWindow: InfoWindow(title: "Your Current Location"),
       ),
     };
