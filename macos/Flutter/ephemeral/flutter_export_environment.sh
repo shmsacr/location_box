10ec148b68b9de6fc6e0db70b55d42034c6725f8
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-12-22 14:10:59 +0300

    Add new plugins and update Flutter paths

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutterprojects\location_box"
+export "FLUTTER_ROOT=/Users/ersinmandaci/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ersinmandaci/GitHub/location_box"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
