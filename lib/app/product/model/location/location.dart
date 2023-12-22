685fcb57e96e8a8509fef1cb548af4fbdcaad58a
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-12-22 03:00:22 +0300

    Refactor Location model and add Hive support

--- lib/app/product/model/location/location.dart
+++ lib/app/product/model/location/location.dart
@@ -1,19 +1,22 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 part 'location.freezed.dart';
 part 'location.g.dart';
 
 @freezed
+@HiveType(typeId: 0)
 class Location with _$Location {
+  @HiveField(0)
   const factory Location({
-    int? id,
-    double? latitude,
-    double? longitude,
-    String? title,
-    String? description,
-    String? picture,
-    String? address,
-    String? phoneNumber,
+    @HiveField(1) int? id,
+    @HiveField(2) double? latitude,
+    @HiveField(3) double? longitude,
+    @HiveField(4) String? title,
+    @HiveField(5) String? description,
+    @HiveField(6) String? picture,
+    @HiveField(7) String? address,
+    @HiveField(8) String? phoneNumber,
   }) = _Location;
 
   factory Location.fromJson(Map<String, Object?> json) =>
