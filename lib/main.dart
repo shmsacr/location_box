1c075f6d1b670b1c262a43fb950c7ad9680f7e91
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-12-22 03:00:27 +0300

    Add Hive initialization and LocationAdapter registration

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:location_box/app/product/init/localization/app_localization.dart';
 import 'package:location_box/app/product/init/state/theme/view_model.dart';
 import 'package:location_box/app/product/init/state_initialize.dart';
+import 'package:location_box/app/product/model/location/location.dart';
 import 'package:location_box/app/product/navigation/app_router.dart';
 import 'package:location_box/app/product/theme/dark_theme_data.dart';
 import 'package:location_box/app/product/theme/light_theme_data.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+   Hive.registerAdapter<Location>(LocationAdapter());
   await EasyLocalization.ensureInitialized();
   runApp(AppLocalization(child: StateInitialize(child: _MyApp())));
 }
