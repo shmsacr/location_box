c78480e607aaeb8e0ef22ccc16714ae672d4c5c2
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-12-22 14:10:43 +0300

    Update GoogleMapsState class with new properties

--- lib/app/view/maps/view_model/state/google_maps_state.dart
+++ lib/app/view/maps/view_model/state/google_maps_state.dart
@@ -2,19 +2,21 @@ import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location_box/app/product/model/location/location.dart';
 
-class GoogleMapsState extends Equatable {
+final class GoogleMapsState extends Equatable {
   final List<Location>? locations;
   final Location? location;
-  final bool? isLoading;
-  final bool? isSaving ;
+  final bool isLoading;
+  final bool isSaving;
+  final bool isDeleting;
   final LatLng? currentLocation;
   final double? latitude;
   final double? longitude;
   GoogleMapsState({
     this.locations,
     this.location,
-    this.isLoading,
-    this.isSaving,
+    this.isLoading = false,
+    this.isSaving = false,
+    this.isDeleting = false,
     this.currentLocation,
     this.latitude,
     this.longitude,
@@ -25,6 +27,7 @@ class GoogleMapsState extends Equatable {
     Location? location,
     bool? isLoading,
     bool? isSaving,
+    bool? isDeleting,
     LatLng? currentLocation,
     double? latitude,
     double? longitude,
@@ -34,6 +37,7 @@ class GoogleMapsState extends Equatable {
       location: location ?? this.location,
       isLoading: isLoading ?? this.isLoading,
       isSaving: isSaving ?? this.isSaving,
+      isDeleting: isDeleting ?? this.isDeleting,
       currentLocation: currentLocation ?? this.currentLocation,
       latitude: latitude ?? this.latitude,
       longitude: longitude ?? this.longitude,
@@ -41,11 +45,11 @@ class GoogleMapsState extends Equatable {
   }
 
   @override
-  // TODO: implement props
   List<Object?> get props => [
         locations,
         location,
         isLoading,
+        isDeleting,
         isSaving,
         currentLocation,
         latitude,
