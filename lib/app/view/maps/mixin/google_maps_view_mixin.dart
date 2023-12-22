8d13dd2b0b2e178f1b7da6369086391b68e656bc
Sehmus Acar <shmsacr@gmail.com>
2023-12-22 18:14:09 +0300

    Refactor GoogleMapsViewMixin in google_maps_view_mixin.dart

--- lib/app/view/maps/mixin/google_maps_view_mixin.dart
+++ lib/app/view/maps/mixin/google_maps_view_mixin.dart
@@ -6,15 +6,7 @@ import 'package:location_box/app/view/maps/view/google_maps_view.dart';
 import 'package:location_box/app/view/maps/view_model/google_maps_view_model.dart';
 
 mixin GoogleMapsViewMixin on State<GoogleMapsView> {
-  final TextEditingController addressController = TextEditingController();
-  final TextEditingController descriptionController = TextEditingController();
-  final TextEditingController imageController = TextEditingController();
-  final TextEditingController phoneController = TextEditingController();
-
-  late GoogleMapController mapController;
-  final TextEditingController titleController = TextEditingController();
-  final formKey = GlobalKey<FormBuilderState>();
-
+  final googleMapsViewModel = GoogleMapsViewModel();
   @override
   void initState() {
     super.initState();
@@ -22,14 +14,19 @@ mixin GoogleMapsViewMixin on State<GoogleMapsView> {
   }
 
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    context.read<GoogleMapsViewModel>().getLocations();
+  }
+
+  @override
   void dispose() {
-    formKey.currentState?.reset();
-    addressController.dispose();
-
-    descriptionController.dispose();
-    imageController.dispose();
-    titleController.dispose();
-
+    googleMapsViewModel.formKey.currentState?.reset();
+    googleMapsViewModel.formKey.currentState?.dispose();
+    
+    googleMapsViewModel.descriptionController.dispose();
+    googleMapsViewModel.imageController.dispose();
+    googleMapsViewModel.titleController.dispose();
     super.dispose();
   }
 
