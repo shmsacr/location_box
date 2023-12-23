288c8ad01c9f415ff6447d01d3a910296693b0ed
Sehmus Acar <shmsacr@gmail.com>
2023-12-23 23:42:04 +0300

    Refactor GoogleMapsViewMixin in google_maps_view_mixin.dart

--- lib/app/view/maps/mixin/google_maps_view_mixin.dart
+++ lib/app/view/maps/mixin/google_maps_view_mixin.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location_box/app/view/maps/view/google_maps_view.dart';
 import 'package:location_box/app/view/maps/view_model/google_maps_view_model.dart';
 
 mixin GoogleMapsViewMixin on State<GoogleMapsView> {
-  final googleMapsViewModel = GoogleMapsViewModel();
-  @override
-  void initState() {
-    super.initState();
-    getLocationMaps();
-  }
+  late final GoogleMapsViewModel _googleMapsViewModel;
+  GoogleMapsViewModel get googleMapsViewModel => _googleMapsViewModel;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    context.read<GoogleMapsViewModel>().getLocations();
+  void initState() {
+    _googleMapsViewModel = context.read<GoogleMapsViewModel>();
+    super.initState();
+    getLocationMaps();
   }
 
   @override
   void dispose() {
     googleMapsViewModel.formKey.currentState?.reset();
     googleMapsViewModel.formKey.currentState?.dispose();
-    
+
     googleMapsViewModel.descriptionController.dispose();
     googleMapsViewModel.imageController.dispose();
     googleMapsViewModel.titleController.dispose();
