69e66e4260287e6a5bb38f2689ede4c3c47599bb
Sehmus Acar <shmsacr@gmail.com>
2023-12-24 03:12:59 +0300

    Update import paths for Google Maps view model

--- lib/app/product/init/state/app/prod_state_get_it.dart
+++ lib/app/product/init/state/app/prod_state_get_it.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:location_box/app/product/init/state/theme/view_model.dart';
-import 'package:location_box/app/view/maps/view_model/google_maps_view_model.dart';
+import 'package:location_box/app/feature/view/maps/view_model/google_maps_view_model.dart';
 
 final class ProductStateGetIt {
   ProductStateGetIt._();
--- lib/app/product/init/state/base/base_state.dart
+++ lib/app/product/init/state/base/base_state.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:location_box/app/product/init/state/app/prod_state_get_it.dart';
 import 'package:location_box/app/product/init/state/theme/view_model.dart';
-import 'package:location_box/app/view/maps/view_model/google_maps_view_model.dart';
+import 'package:location_box/app/feature/view/maps/view_model/google_maps_view_model.dart';
 
 abstract class BaseState<T extends StatefulWidget> extends State<T> {
   AppThemeViewModel get appThemeViewModel =>
