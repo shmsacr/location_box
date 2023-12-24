91196cf1b2c9114fe1b37f8749c8434bcf39171e
Sehmus Acar <shmsacr@gmail.com>
2023-12-24 03:10:38 +0300

    Add location service and implementation, remove location storage

---
+++ lib/app/core/service/location_service/location_service_impl.dart
@@ -0,0 +1,33 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:location_box/app/core/service/location_service/location_service.dart';
+
+class LocationServiceImpl extends LocationService {
+  @override
+  Future<String> getAddressFromLatLng(
+      {required double latitude, required double longitude}) async {
+    try {
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(latitude, longitude);
+      if (placemarks.isNotEmpty) {
+        Placemark place = placemarks[0];
+        return "${place.street}, ${place.locality}, ${place.postalCode}, ${place.country}";
+      }
+      return "Adres bulunamadi";
+    } catch (e) {
+      throw Exception("Adres çevirme hatasi: $e");
+    }
+  }
+
+  @override
+  Future<Position> getCurrentPosition() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low,
+      );
+      return position;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
--- lib/app/core/service/location_service/location_storage.dart
+++
@@ -1,9 +0,0 @@
-import '../../../product/model/location/location.dart';
-
-abstract class LocationStorage {
-  Future<bool> addLocation({required Location location});
-  Future<Location> getLocation({required Location location});
-  Future<List<Location>> getAllLocations();
-  Future<bool> deleteLocation({required Location location});
-  Future<Location> updateLocation({required Location location});
-}
