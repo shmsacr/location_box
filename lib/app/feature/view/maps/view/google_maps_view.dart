5f071a639fd81ec37aa0948640dff4c3a2dd66e6
Sehmus Acar <shmsacr@gmail.com>
2024-01-02 05:37:43 +0300

    Add import for dart:io and modify saveLocation() to accept an imageFile parameter

--- lib/app/feature/view/maps/view/google_maps_view.dart
+++ lib/app/feature/view/maps/view/google_maps_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -128,7 +130,7 @@ class _GoogleMapsViewState extends State<GoogleMapsView>
                                             onPressed: () {
                                               context
                                                   .read<GoogleMapsViewModel>()
-                                                  .saveLocation();
+                                                  .saveLocation(imageFile);
                                               if (state.isSaving) {
                                                 print(
                                                     'state : ${state.locations}');
