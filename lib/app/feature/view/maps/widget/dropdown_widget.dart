d7ec229428f43d69a3a8f2bbc40022f0c9de4b24
Sehmus Acar <shmsacr@gmail.com>
2024-01-05 17:36:58 +0300

    Refactor dropdown widget to use iconController

--- lib/app/feature/view/maps/widget/dropdown_widget.dart
+++ lib/app/feature/view/maps/widget/dropdown_widget.dart
@@ -2,34 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:location_box/app/feature/view/maps/enum/markes_icon_enum.dart';
 
 class CustomDropDownWidget extends StatefulWidget {
-  const CustomDropDownWidget({super.key});
+  const CustomDropDownWidget({super.key, required this.iconController});
+  final TextEditingController? iconController;
 
   @override
   State<CustomDropDownWidget> createState() => _CustomDropDownWidgetState();
 }
 
 class _CustomDropDownWidgetState extends State<CustomDropDownWidget> {
-  final TextEditingController iconController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return DropdownMenu<MarkerIcons>(
-        controller: iconController,
-        enableFilter: true,
-        requestFocusOnTap: true,
-        leadingIcon: const Icon(Icons.search),
+        initialSelection: MarkerIcons.ic_default,
+        controller: widget.iconController,
+        leadingIcon: widget.iconController?.value.text != null
+            ? Image.asset(MarkerIcons.ic_default.value, width: 30, height: 30)
+            : Image.asset(MarkerIcons.ic_default.value, width: 30, height: 30),
         label: const Text('Icon'),
         inputDecorationTheme: const InputDecorationTheme(
           filled: true,
           contentPadding: EdgeInsets.symmetric(vertical: 5.0),
         ),
-        onSelected: (MarkerIcons? icon) {},
+        onSelected: (MarkerIcons? icon) {
+          if (icon != null) {
+            widget.iconController?.value = TextEditingValue(text: icon.value);
+          }
+        },
         dropdownMenuEntries:
             MarkerIcons.values.map<DropdownMenuEntry<MarkerIcons>>(
           (MarkerIcons icon) {
             return DropdownMenuEntry<MarkerIcons>(
               value: icon,
               label: icon.key,
-              leadingIcon: Image.asset(icon.value),
+              leadingIcon: Image.asset(icon.value, width: 30, height: 30),
             );
           },
         ).toList());
