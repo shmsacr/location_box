6b3285fe484923f4dfbf7bce88855c1959c9c495
Sehmus Acar <shmsacr@gmail.com>
2024-01-14 23:13:03 +0300

    Add GetIt and MyViewModel to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:location_box/app/product/init/localization/app_localization.dart';
 import 'package:location_box/app/product/init/state/theme/view_model.dart';
 import 'package:location_box/app/product/init/state_initialize.dart';
 import 'package:location_box/app/product/model/location/location_model.dart';
+import 'package:location_box/app/product/model/my_view_model.dart';
 import 'package:location_box/app/product/navigation/app_router.dart';
 import 'package:location_box/app/product/theme/dark_theme_data.dart';
 import 'package:location_box/app/product/theme/light_theme_data.dart';
 
+
 Future<void> main() async {
+  GetIt getIt = GetIt.instance;
+  getIt.registerSingleton<MyViewModel>(MyViewModel(), signalsReady: true);
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter<LocationModel>(LocationModelAdapter());
