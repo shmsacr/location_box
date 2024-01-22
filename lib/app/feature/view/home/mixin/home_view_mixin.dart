35dd0956050289d18caba422cd3751b639648171
Sehmus Acar <shmsacr@gmail.com>
2024-01-23 02:30:05 +0300

    Add HomeViewMixin to home_view.dart

---
+++ lib/app/feature/view/home/mixin/home_view_mixin.dart
@@ -0,0 +1,22 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:location_box/app/feature/view/home/view/home_view.dart';
+import 'package:location_box/app/feature/view/home/view_model/home_view_model.dart';
+
+mixin HomeViewMixin on State<HomeView>{
+  final TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
