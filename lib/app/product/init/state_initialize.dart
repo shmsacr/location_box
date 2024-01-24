df2a53828711883801951473cb7a07c9922ca70f
Sehmus Acar <shmsacr@gmail.com>
2024-01-24 15:20:52 +0300

    Update imports and initialize HomeViewModel with getLocations()

--- lib/app/product/init/state_initialize.dart
+++ lib/app/product/init/state_initialize.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:location_box/app/feature/view/home/view/home_view.dart';
 import 'package:location_box/app/feature/view/home/view_model/home_view_model.dart';
-import 'package:location_box/app/product/init/state/theme/view_model.dart';
 import 'package:location_box/app/feature/view/maps/view_model/google_maps_view_model.dart';
+import 'package:location_box/app/product/init/state/theme/view_model.dart';
 
 final class StateInitialize extends StatelessWidget {
   const StateInitialize({required this.child, super.key});
@@ -18,7 +17,8 @@ final class StateInitialize extends StatelessWidget {
         BlocProvider<GoogleMapsViewModel>.value(
           value: GoogleMapsViewModel()..getLocations(),
         ),
-        BlocProvider<HomeViewModel>.value(value: HomeViewModel()),
+        BlocProvider<HomeViewModel>.value(
+            value: HomeViewModel()..getLocations()),
       ],
       child: child,
     );
