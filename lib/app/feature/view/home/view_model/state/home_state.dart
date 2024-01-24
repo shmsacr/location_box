d8c53c66a198218d1ebe646e6af1def3c1d2653d
Sehmus Acar <shmsacr@gmail.com>
2024-01-24 15:20:42 +0300

    Add lastSearchLength property to HomeState

--- lib/app/feature/view/home/view_model/state/home_state.dart
+++ lib/app/feature/view/home/view_model/state/home_state.dart
@@ -6,12 +6,14 @@ final class HomeState extends Equatable {
   final bool isLoading;
   final List<LocationModel>? searchMaps;
   final List<LocationModel>? locations;
+  final int lastSearchLenght;
 
   HomeState({
     this.isLoading = false,
     this.isSearching = false,
     this.searchMaps,
     this.locations,
+    this.lastSearchLenght = 0,
   });
 
   HomeState copyWith({
@@ -19,15 +21,17 @@ final class HomeState extends Equatable {
     bool? isLoading,
     List<LocationModel>? searchMaps,
     List<LocationModel>? locations,
+    int? lastSearchLenght,
   }) {
     return HomeState(
       isSearching: isSearching ?? this.isSearching,
       isLoading: isLoading ?? this.isLoading,
       searchMaps: searchMaps ?? this.searchMaps,
       locations: locations ?? this.locations,
+      lastSearchLenght: lastSearchLenght ?? this.lastSearchLenght,
     );
   }
 
   @override
-  List<Object?> get props => [isSearching, searchMaps, locations, isLoading];
+  List<Object?> get props => [isSearching, searchMaps, locations, isLoading, lastSearchLenght];
 }
