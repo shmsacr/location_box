203b9b5381c95db05dc840dee975d2805d8f03cf
Sehmus Acar <shmsacr@gmail.com>
2024-01-27 21:42:32 +0300

    Update GoogleMapsViewMixin imports and variables

--- lib/app/feature/view/maps/mixin/google_maps_view_mixin.dart
+++ lib/app/feature/view/maps/mixin/google_maps_view_mixin.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import 'package:location_box/app/feature/view/home/view_model/home_view_model.dart';
 import 'package:location_box/app/feature/view/maps/view/google_maps_view.dart';
 import 'package:location_box/app/feature/view/maps/view_model/google_maps_view_model.dart';
 import 'package:location_box/app/feature/view/maps/widget/custom_info_windows.dart';
@@ -12,24 +13,19 @@ import 'package:path/path.dart' as path;
 import 'package:permission_handler/permission_handler.dart';
 
 mixin GoogleMapsViewMixin on State<GoogleMapsView> {
-  late final GoogleMapsViewModel _googleMapsViewModel;
-  GoogleMapsViewModel get googleMapsViewModel => _googleMapsViewModel;
+  late final HomeViewModel _googleMapsViewModel;
+  HomeViewModel get googleMapsViewModel => _googleMapsViewModel;
   File? imageFile;
 
   @override
   void initState() {
-    _googleMapsViewModel = context.read<GoogleMapsViewModel>();
+    _googleMapsViewModel = context.read<HomeViewModel>();
     super.initState();
-    getLocationMaps();
   }
 
   @override
   void dispose() {
     super.dispose();
-  }
-
-  Future<void> getLocationMaps() async {
-    await context.read<GoogleMapsViewModel>().getCurrentLocation();
   }
 
   Set<Marker> createMarker({required LatLng position}) {
@@ -41,6 +37,4 @@ mixin GoogleMapsViewMixin on State<GoogleMapsView> {
       ),
     };
   }
-
-  
 }
