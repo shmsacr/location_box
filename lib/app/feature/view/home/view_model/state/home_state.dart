c09d47523b272daa4196f18c8c7d06861ecc18b1
Sehmus Acar <shmsacr@gmail.com>
2024-01-27 21:42:00 +0300

    Add location-related properties to HomeState class

--- lib/app/feature/view/home/view_model/state/home_state.dart
+++ lib/app/feature/view/home/view_model/state/home_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location_box/app/product/model/location/location_model.dart';
 
 final class HomeState extends Equatable {
@@ -7,6 +8,13 @@ final class HomeState extends Equatable {
   final List<LocationModel>? searchMaps;
   final List<LocationModel>? locations;
   final int lastSearchLenght;
+  final LocationModel? location;
+  final bool isSaving;
+  final bool isDeleting;
+  final LatLng? currentLocation;
+  final double? latitude;
+  final double? longitude;
+  final List<Marker>? markers;
 
   HomeState({
     this.isLoading = false,
@@ -14,6 +22,14 @@ final class HomeState extends Equatable {
     this.searchMaps,
     this.locations,
     this.lastSearchLenght = 0,
+    this.location,
+    this.isSaving = false,
+    this.isDeleting = false,
+    this.currentLocation,
+    this.latitude,
+    this.longitude,
+    this.markers,
+
   });
 
   HomeState copyWith({
@@ -22,6 +38,13 @@ final class HomeState extends Equatable {
     List<LocationModel>? searchMaps,
     List<LocationModel>? locations,
     int? lastSearchLenght,
+    LocationModel? location,
+    bool? isSaving,
+    bool? isDeleting,
+    LatLng? currentLocation,
+    double? latitude,
+    double? longitude,
+    List<Marker>? markers,
   }) {
     return HomeState(
       isSearching: isSearching ?? this.isSearching,
@@ -29,9 +52,17 @@ final class HomeState extends Equatable {
       searchMaps: searchMaps ?? this.searchMaps,
       locations: locations ?? this.locations,
       lastSearchLenght: lastSearchLenght ?? this.lastSearchLenght,
+      location: location ?? this.location,
+      isSaving: isSaving ?? this.isSaving,
+      isDeleting: isDeleting ?? this.isDeleting,
+      currentLocation: currentLocation ?? this.currentLocation,
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
+      markers: markers ?? this.markers,
+
     );
   }
 
   @override
-  List<Object?> get props => [isSearching, searchMaps, locations, isLoading, lastSearchLenght];
+  List<Object?> get props => [isSearching, searchMaps, locations, isLoading, lastSearchLenght, location, isSaving, isDeleting, currentLocation, latitude, longitude, markers,];
 }
